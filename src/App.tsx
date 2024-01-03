df6da26a554b7351dd0e5598cf851b66c7a20e5a
dz-vadim <46889822+dz-vadim@users.noreply.github.com>
2024-01-03 17:53:21 +0200

    Clean app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
 import React from 'react';
 import "./styles/App.css";
 import TodoContainer from "./components/TodoContainer";
-import SummaryTable from "./components/SummaryTable";
-
 const App = () => {
-    // var request = require("request");
-    //
-    // var options = { method: 'GET',
-    //     url: 'https://robonotes-5f3c.restdb.io/rest/todos',
-    //     headers:
-    //         { 'cache-control': 'no-cache',
-    //             'x-apikey': '69f2ef2a30077e770a6c720cd506045527d40' } };
-    //
-    // request(options, function (error: string, response : string, body: string) {
-    //     if (error) throw new Error(error);
-    //
-    //     console.log(body);
-    // });
 
     return (
         <div>
